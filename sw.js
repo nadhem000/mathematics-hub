74ad49951b98b3262291f3b4bf9e66a7f24c4114
MejriZiad <mejri.ziad@gmail.com>
2025-11-14 13:40:56 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Mathematics Hub - Enhanced Version
-const CACHE_NAME = 'math-hub-v1.0.5';
+const CACHE_NAME = 'math-hub-v1.0.6';
 const urlsToCache = [
   '/',
   '/index.html',
